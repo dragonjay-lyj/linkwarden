6af4c00704498074c713998bf1c7057ee0b96d10
李京彧 <liujinlisb@gmail.com>
2024-07-19 22:30:07 +0800

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
+      <script defer src="https://status.dragonjay.top/script.js" data-website-id="ad0a8905-fbd1-467e-86e1-7c3c5fdb813c"></script>
       <Head />
       <body>
         <Main />
