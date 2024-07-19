38ea1ee1f0a24ab9b00d6d6fa14c2c3e94438002
李京彧 <liujinlisb@gmail.com>
2024-07-19 22:31:16 +0800

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,8 +3,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <script defer src="https://status.dragonjay.top/script.js" data-website-id="ad0a8905-fbd1-467e-86e1-7c3c5fdb813c"></script>
-      <Head />
+      <Head>
+        {/* 在Head内添加脚本通常是为了更好的性能和加载控制 */}
+        <script
+          defer
+          src="https://status.dragonjay.top/script.js"
+          data-website-id="ad0a8905-fbd1-467e-86e1-7c3c5fdb813c"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
